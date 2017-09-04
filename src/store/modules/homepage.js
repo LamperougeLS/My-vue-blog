13e7d034d4abc367c9bce6e0bac22928faa50710
Lamperouge <178848449@qq.com>
2017-09-04 23:57:42 +0800

    null

--- src/store/modules/homepage.js
+++ src/store/modules/homepage.js
@@ -1,16 +1,29 @@
-import
+import * as types from '../mutation-types.js'
 
 const state = {
-  tagList: '',
-  articleList: ''
+  tagList: [{ name: '全部', id: 'all' },
+    { name: 'VUE', id: 'vue' },
+    { name: 'REACT', id: 'react' },
+    { name: '热门', id: 'hot' },
+    { name: 'ES6', id: 'es6' },
+    { name: 'WEBPACK', id: 'webpack' },
+    { name: 'REACT', id: 'react' },
+    { name: '热门', id: 'hot' },
+    { name: 'ES6', id: 'es6' }
+  ],
+  articleList: [{ title: '文章saddasfdASD', tag: 'ASDASD', pageview: '1233', date: '2017-01-01', abstract: 'asfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfaasfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpassdasdasdasdafjasfsfasfasifjoasjfapofjpassdasdaasfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpassdasdasdasdafjasfsdasdafjasf' },
+    { title: '文章saddasfdASD', tag: 'ASDASD', pageview: '1233', date: '2017-01-01', abstract: 'aasfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpassdasdasdasdafjasfsfhoaihfoiahasfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpassdasdasdasdafjasfsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpasfjsdsdsadaasf' },
+    { title: '文章saddasfdASD', tag: 'ASDASD', pageview: '1233', date: '2017-01-01', abstract: 'asfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaasfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpassdasdasdasdafjasfishfoiasfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpassdasdasdasdafjasfasfhfasfasfasfasifjoasjfapofjpasdasdasasfjasf' },
+    { title: '文章saddasfdASD', tag: 'ASDASD', pageview: '1233', date: '2017-01-01', abstract: 'asfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpaasdasdasdsfasfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpassdasdasdasdafjasfasfhoaihfoiahsfoiahsfiohaosifhoaisjfoiashfoasfhoaishfoiasfhfasfasfasfasifjoasjfapofjpassdasdasdasdafjasfjasf' }
+  ]
 }
 
 const mutations = {
-  [GET_ARTICLE](state, data) {
+  [types.GET_ARTICLE] (state, data) {
     state.tagList = data.tagList
     state.articleList = data.articleList
   },
-  [GET_ARTICLE_FAILURE](state) {
+  [types.GET_ARTICLE_FAILURE] (state) {
     return state
   }
 }
